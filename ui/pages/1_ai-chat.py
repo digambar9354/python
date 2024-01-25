212ad7768ea26d7e690cf7b25421bc2b2e6aab77
SolverBeeDev01 <112374907+SolverBeeDev01@users.noreply.github.com>
2024-01-25 22:50:58 +0530

    Chat bot changes

--- ui/pages/1_ai-chat.py
+++ ui/pages/1_ai-chat.py
@@ -1,8 +1,48 @@
 import streamlit as st
+import os
 from streamlit_chat import message
+
+from langchain.prompts.chat import (
+    ChatPromptTemplate,
+    HumanMessagePromptTemplate,
+    SystemMessagePromptTemplate,
+)
+from langchain.schema import HumanMessage, SystemMessage
+from langchain_openai import ChatOpenAI
+
+os.environ["LANGCHAIN_API_KEY"] = "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"
+chat = ChatOpenAI(temperature=0)
+messages = []
 
 def on_input_change():
     user_input = st.session_state.user_input
+
+    if len(st.session_state.generated) > 0:
+        messages.append(
+            SystemMessage(
+                content="You are a helpful assistant that helps to solve the user query."
+            )
+        )
+
+    messages.append(
+        HumanMessage(
+            content=user_input
+        )
+    )
+    
+    
+    systemMessage = chat(messages)
+    
+    
+    messages.append(
+        SystemMessage(
+            content=systemMessage.content
+        )
+    )
+    
+    st.write("### chat  messages:")
+    st.write(systemMessage)
+    
     st.session_state.past.append(user_input)
     st.session_state.generated.append({'type': 'normal', 'data': 'The messages from Bot\nWith new line'})
 
@@ -10,18 +50,8 @@ def on_btn_click():
     del st.session_state.past[:]
     del st.session_state.generated[:]
 
-st.session_state.setdefault(
-    'past', 
-    [
-        'plan text with line break'
-    ]
-)
-st.session_state.setdefault(
-    'generated', 
-    [
-        {'type': 'normal', 'data': 'Line 1 \n Line 2 \n Line 3'}
-    ]
-)
+st.session_state.setdefault('past', [])
+st.session_state.setdefault('generated', [])
 
 st.title("Chat placeholder")
 
@@ -29,7 +59,6 @@ chat_placeholder = st.empty()
 
 with chat_placeholder.container():    
     for i in range(len(st.session_state['generated'])):   
-        st.text(f'asdas, [{st.session_state['generated'][i]}]')             
         message(st.session_state['past'][i], is_user=True, key=f"{i}_user")
         message(
             st.session_state['generated'][i]['data'], 
